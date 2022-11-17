7654faf76707aa248a111ec622068da6f7ff022e
mattbzca <111474919+mattbzca@users.noreply.github.com>
2022-11-16 21:01:00 -0800

    Update

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-//1.
-//2.
-//3.
+//1. instantiate a new array and add 5 numeric values to it.
+let list = [3, 7, 9, 1, 10];
+console.log(list[0]);
+console.log(list[4]); //U had to pass through 3, 7, 9, and 5 to get to the last value.
+//2. Using a for loop containing a console.log(...) statement, output each of the values in the array.
+let myNum = [ 54, 25, 92, 77 ]; {
+    for (let i = 0; i < myNum.length; i++)
+}
+    
+    
+
+
+//3. Use the push() method to add 5 more values to your array
+myNum.push(1, 2, 3, 4, 5);
+console.log(myNum); // There are 5 new values, arranged in order after the original array.
+console.log(myNum.pop());
+console.log(myNum.pop());
+console.log(myNum.pop());
 //4.
